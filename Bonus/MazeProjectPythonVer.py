cfd24a1cd8eaee0f6b14640dc54bbd300eb7a91b
Cheese Guy <117108204+CHEEZ-ITZ-12oz@users.noreply.github.com>
2024-11-22 15:37:22 -0800

    Update MazeProjectPythonVer.py
    Finally got the changes in

--- Bonus/MazeProjectPythonVer.py
+++ Bonus/MazeProjectPythonVer.py
@@ -1,1 +1,26 @@
-map = "cant be bothered right now. ill wait for the map from vscode, but i can wait for now"
+map = [["S","W"],["S"],["S"],["N"],["B"],["B"],["N","S","E","W"],["N","W"],["N","E"]]
+
+sendInCommands = ["F","R","L","B","SCN","RUN"]
+Commands = ["CLR","DEL","MOD"]
+
+plrInput = True
+plrList = []
+dummy = ""
+
+while "RUN" not in plrList:
+    dummy = input("Enter Input: ")
+    dummy = dummy.upper()
+    # Send-ins
+    if dummy in sendInCommands:
+        plrList.append(dummy)
+    #Commands
+        dummy_c = dummy[0:2]
+    elif dummy_c in Commands:
+        if dummy_c == "CLR":
+            plrList = []
+        elif dummy_c == "DEL":
+            plrList = plrList[0,-1]
+
+plrList = plrList[0,-1]
+print("Done")
+print(plrList)
