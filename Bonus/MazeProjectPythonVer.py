eaeaad3142dd58019e5d1bd51fe3da1d577057cd
CHEEZ-ITZ-12oz <117108204+CHEEZ-ITZ-12oz@users.noreply.github.com>
2024-11-23 09:42:39 -0800

    Update MazeProjectPythonVer.py
    Development continues 11/23

--- Bonus/MazeProjectPythonVer.py
+++ Bonus/MazeProjectPythonVer.py
@@ -1,26 +1,44 @@
+import numbers
 map = [["S","W"],["S"],["S"],["N"],["B"],["B"],["N","S","E","W"],["N","W"],["N","E"]]
 
 sendInCommands = ["F","R","L","B","SCN","RUN"]
 Commands = ["CLR","DEL","MOD"]
+jumpValCommands = ["VAL","VMD","JMP","JEZ","JNZ","JAV"]
 
-plrInput = True
+
 plrList = []
 dummy = ""
 
 while "RUN" not in plrList:
-    dummy = input("Enter Input: ")
-    dummy = dummy.upper()
-    # Send-ins
-    if dummy in sendInCommands:
-        plrList.append(dummy)
-    #Commands
-        dummy_c = dummy[0:2]
-    elif dummy_c in Commands:
-        if dummy_c == "CLR":
-            plrList = []
-        elif dummy_c == "DEL":
-            plrList = plrList[0,-1]
+    try:
+        dummy = input("Enter Input: ")
+        dummy = dummy.upper()
+        # Send-ins
+        if dummy in sendInCommands:
+            plrList.append(dummy)
+        # Editing Commands
+        elif dummy[0:3] in Commands:
+            if dummy == "CLR":
+                plrList = []
+            elif dummy == "DEL":
+                plrList = plrList[0:-1]
+            elif dummy[0:3] == "MOD":
+                commandval = int(dummy[4:-1])
+                if isinstance(commandval, numbers.Number):
+                    plrList.insert(commandval,input(f"Replace Line {commandval} ({plrList.pop(commandval)}) with: "))
+        # Jump and Values
+        elif dummy[0:3] in jumpValCommands:
+            print("Finish this later")
+        
+        
+        
+        print("Commands:")
+        for i in range(len(plrList)):
+            print(f"{i}. {plrList[i]}")
+    except:
+        print("Invalid Syntax")
+        
 
-plrList = plrList[0,-1]
+plrList = plrList[0:-1]
 print("Done")
 print(plrList)
