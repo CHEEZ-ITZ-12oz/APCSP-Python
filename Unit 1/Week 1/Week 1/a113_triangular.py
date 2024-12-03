925b6c5b00f135961cc362e98a0710d920e1e6b8
CHEEZ-ITZ-12oz <117108204+CHEEZ-ITZ-12oz@users.noreply.github.com>
2024-12-03 11:54:29 -0800

    Add files via upload

---
+++ Unit 1/Week 1/Week 1/a113_triangular.py
@@ -0,0 +1,12 @@
+#   a113_numeric_comparison.py
+#   Predict what the following code will do.
+import turtle as trtl
+
+pen = trtl.Turtle()
+
+for line in range(3):
+  pen.forward(100)
+  pen.right(120)
+
+wn = trtl.Screen()
+wn.mainloop()
