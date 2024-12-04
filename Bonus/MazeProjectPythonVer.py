f1c4d4124f551746b594c7770495d7b0b7d7584d
CHEEZ-ITZ-12oz <117108204+CHEEZ-ITZ-12oz@users.noreply.github.com>
2024-12-04 12:44:26 -0800

    Update MazeProjectPythonVer.py
    Update as of 12/4

--- Bonus/MazeProjectPythonVer.py
+++ Bonus/MazeProjectPythonVer.py
@@ -2,7 +2,7 @@ import math as math
 import numbers
 map = [["S","W"],["S"],["S"],["N"],[],["E"],["N","S","E","W"],["N","W"],["N","E"]]
 
-sendInCommands = ["F","R","L","B","SCN","RUN"]
+sendInCommands = ["F","B","L","R","SCN","RUN"]
 Commands = ["CLR","DEL","MOD"]
 jumpValCommands = ["VAL","VMD","JMP","JEZ","JNZ","JAV"]
 
@@ -20,55 +20,65 @@ def validCmd(cmd):
     else:
         return False
 
+def turnPlayer(direction):
+    if direction == "R":
+        Facing = (Facing + 1) % 4
+    elif direction == "L":
+        Facing = (Facing - 1) % 4
+
 # Innitialize
 mapLength = int(math.sqrt(len(map)))
 movements = {
-    "F": mapLength,
-    "B": -mapLength,
-    "L": -1,
-    "R": 1}
-collisionCheck = {
-    "F": "N",
-    "B": "S",
-    "L": "W",
-    "R": "E"}
+    0: mapLength,
+    1: -mapLength,
+    2: -1,
+    3: 1}
 jumpCheckers = {
     "JMP": lambda: True,
     "JEZ": lambda: (plrVariable == 0),
     "JNZ": lambda: (plrVariable != 0),
     "JAV": lambda: True}
+directionDefs = {
+    0: "N",
+    1: "E",
+    2: "S",
+    3: "W"}
 plrList = []
 
 while True: # Game Loop
     dummy = ""
+    # Get Inputs
     while "RUN" not in plrList:
-        dummy = input("Enter Input: ")
-        dummy = dummy.upper()
-        # Editing
-        if dummy[0:3] in Commands:
-            if dummy == "CLR":
-                plrList = []
+        try:
+            dummy = input("Enter Input: ")
+            dummy = dummy.upper()
+            # Editing
+            if dummy[0:3] in Commands:
+                if dummy == "CLR":
+                    plrList = []
+                    updateCmdBoard()
+                elif dummy == "DEL":
+                    plrList = plrList[0:-1]
+                    updateCmdBoard()
+                elif dummy[0:3] == "MOD":
+                    commandval = int(dummy[4:-1])
+                    if Isnumber(commandval):
+                        dummy = input(f"Replace Line {commandval} ({plrList[commandval]}) with: ")
+                        dummy = dummy.upper()
+                        if validCmd(dummy):
+                            plrList[commandval]=dummy
+                            updateCmdBoard()
+                        else:
+                            updateCmdBoard()
+                            print("Invalid Input")
+            elif validCmd(dummy):
+                plrList.append(dummy)
                 updateCmdBoard()
-            elif dummy == "DEL":
-                plrList = plrList[0:-1]
+            else:
                 updateCmdBoard()
-            elif dummy[0:3] == "MOD":
-                commandval = int(dummy[4:-1])
-                if Isnumber(commandval):
-                    dummy = input(f"Replace Line {commandval} ({plrList[commandval]}) with: ")
-                    dummy = dummy.upper()
-                    if validCmd(dummy):
-                        plrList[commandval]=dummy
-                        updateCmdBoard()
-                    else:
-                        updateCmdBoard()
-                        print("Invalid Input")
-        elif validCmd(dummy):
-            plrList.append(dummy)
-            updateCmdBoard()
-        else:
-            updateCmdBoard()
-            print("Invalid Input")
+                print("Invalid Input")
+        except Exception:
+            print("Invalid Format. Insert Commands as cmd(x)")
     # Run
     plrList = plrList[0:-1]
     updateCmdBoard()
@@ -77,28 +87,40 @@ while True: # Game Loop
     index = 0
     Position = 0
     plrVariable = 0
+    Facing = 0
     TurnReporter = []
     while index < len(plrList): 
         cmd = plrList[index]
         index += 1
         # Movment
-        if collisionCheck.get(cmd,"Zeta!!!!") not in map[Position]:
-            Position += movements.get(cmd,0)
-        elif movements.get(cmd,0) != 0:
-            TurnReporter = ["Crashed"]
-            break
-        if cmd == "SCN":
+        if cmd in ["L","R"]:
+            turnPlayer(cmd)
+        elif cmd == "F":
+            if directionDefs.get(Facing) not in map[Position]:
+                Position += movements.get(Facing,0)
+            else:
+                TurnReporter = ["Crashed"]
+                break
+        elif cmd == "B":
+            if directionDefs.get((Facing+2)%4) not in map[Position]:
+                Position += movements.get((Facing+2)%4,0)
+            else:
+                TurnReporter = ["Crashed"]
+                break
+        elif cmd == "SCN":
             TurnReporter.append(map[Position])
-        # Jump/Var Detection and var Commands    
+        # Jump/Var Detection and var Commands  
+        # This formats the command  
         if cmd[0:3] in jumpValCommands:
             runval = int(cmd[4:-1])
             cmd = cmd[0:3]
+        # Commands
         if cmd == "VAL":
             plrVariable = runval
         elif cmd == "VMD":
             plrVariable += runval
         # Jump Commands
-        elif jumpCheckers.get(cmd,lambda: False)():
+        elif jumpCheckers.get(cmd,lambda:False)():
             LoopCount += 1
             index = runval
             if cmd == "JAV":
@@ -109,5 +131,5 @@ while True: # Game Loop
 
     TurnReporter.append("Complete")
     for i in TurnReporter: print(i)
-    if input("Type Anything to Continue: ") == "exit": break
+    if input("Continue? (y/n): ") == "n": break
     updateCmdBoard()
