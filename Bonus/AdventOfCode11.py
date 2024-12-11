94c5cc845f2ebecd0af6b2c5a91b0c637975cfcc
CHEEZ-ITZ-12oz <117108204+CHEEZ-ITZ-12oz@users.noreply.github.com>
2024-12-11 12:15:41 -0800

    Create AdventOfCode11.py
    partially

---
+++ Bonus/AdventOfCode11.py
@@ -0,0 +1,8 @@
+RockPusher = [5, 127, 680267, 39260, 0, 26, 3553, 5851995]
+for loop in range(75):
+  print(loop)
+  SchrodingersRockPile = []
+  for i in RockPusher:
+    if i == 0:
+      SchrodingersRockPile.append(1)
+    elif 
