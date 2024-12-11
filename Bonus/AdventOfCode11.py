e041ac6bdc328441e20dea82fa435dac48fe7200
CHEEZ-ITZ-12oz <117108204+CHEEZ-ITZ-12oz@users.noreply.github.com>
2024-12-11 12:48:47 -0800

    Update AdventOfCode11.py
    finished program

--- Bonus/AdventOfCode11.py
+++ Bonus/AdventOfCode11.py
@@ -1,8 +1,23 @@
-RockPusher = [5, 127, 680267, 39260, 0, 26, 3553, 5851995]
-for loop in range(75):
-  print(loop)
-  SchrodingersRockPile = []
-  for i in RockPusher:
-    if i == 0:
-      SchrodingersRockPile.append(1)
-    elif 
+
+RockPusher = ["5", "127", "680267", "39260", "0", "26", "3553", "5851995"]
+for loop in range(25):
+    print(loop)
+    SchrodingersRockPile = []
+    for i in RockPusher:
+        if i == "0":
+            SchrodingersRockPile.append("1")
+        elif len(i)%2 == 0:
+            half = len(i)//2
+            add1 = i[0:half]
+            add2 = str(int(i[half:])*1)
+            SchrodingersRockPile.append(add1)
+            SchrodingersRockPile.append(add2)
+        else:
+            addelse = str(int(i)*2024)
+            SchrodingersRockPile.append(addelse)
+    RockPusher = SchrodingersRockPile
+    SchrodingersRockPile = []
+
+
+TellSchrodinger___iLived = len(RockPusher)
+print(TellSchrodinger___iLived)
