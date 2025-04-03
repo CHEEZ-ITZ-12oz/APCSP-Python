cb16d4476b62d08e29d9d56cb90b19fb76babb4e
CHEEZ-ITZ-12oz <117108204+CHEEZ-ITZ-12oz@users.noreply.github.com>
2025-04-03 15:44:05 -0700

    Update timers.py

--- Unit 2/AP Project/timers.py
+++ Unit 2/AP Project/timers.py
@@ -1,41 +1,11 @@
-import turtle as trtl, string, random
+## .......... SETUP .......... ##
+import turtle as trtl, string, datetime
 
-pen = trtl.Turtle()
+# setup - screen
 wn = trtl.Screen()
 trtl.tracer(0)
 
-bts = []
-for i in range(10):
-    temp = trtl.Turtle(shape="circle")
-    temp.penup()
-    temp.hideturtle()
-    bts.append(temp)
-
-pen.hideturtle()
-pen.penup()
-pen.goto(0,200)
-
-letters = list(string.printable) + ["Return","BackSpace"," ","Shift_L","Shift_R"]
-numbers = list(string.digits)
-numbers += ["/","BackSpace","Return","Escape"]
-
-dayspermonth=[0,31,28,31,30,31,30,30,31,30,31,30,31]
-
-
-
-
-
-timers = []
-with open("assets/timers/timerlist.txt","r") as file1:
-        for line in file1:
-            tmplist = line.split(";")
-            timers.append(tmplist)
-
-
-
-
-
-
+## ........ FUNCTIONS ........ ##
 def enterUserText(let,message,valids):
     global stopped, answer, userinput
     alttext = False
@@ -97,8 +67,8 @@ def getInput(question,Inputs=[],cond=[]):
     return answer
 
 
-def maketimer(name,end,start):
-    return([name,end,start])
+def maketimer(name,end):
+    return([name,end])
 
 def getname(timer):
     return(timer[0])
@@ -106,49 +76,83 @@ def getname(timer):
 def getend(timer,islist=False):
     return checkvaliddate(timer[1],True) if islist else timer[1]
 
-def getstart(timer):
-    return(timer[2])
-
 
 def savethedate():
     with open ("assets/timers/timerlist.txt","w") as file1:
         for i in range(len(timers)):
             line = timers[i]
-            line = f"{line[0]};{line[1]};{line[2]}"
+            line = f"{line[0]};{line[1]}\n"
             file1.write(f"{line}")
+
+def isleapyear(year):
+    return (year%4==0 and year%100!=0) or (year%400==0)
 
 
 def checkvaliddate(input,returndate=False):
     try:
-        data = input.split("/")
+        dateordering = []
+        if "/" in input:
+            data = input.split("/") # From user input
+            dateordering = [0,1,2]
+        elif "-" in input:
+            data = input.split("-") # From datetime value
+            dateordering = [1,2,0]
+        else:
+            return False
         if len(data) != 3:
             return False
-        month = int(data[0])
-        if month-1 not in range(12):
-            print("false")
+        month = int(data[dateordering[0]])
+        day = int(data[dateordering[1]])
+        year = int(data[dateordering[2]])
+        if (month-1 not in range(12)) or ( # checks if the inputed date is a real and valid date. Is it a valid month? (1-12)
+            day-1 not in range(dayspermonth[month-1] + (1 if (isleapyear(year) and month == 2) else 0) ) # is the day in the month? 
+            ) or not (0 <= year <= 10000): # is the year out of bounds
             return False
-        day = int(data[1])
-        if day-1 not in range(dayspermonth[month]):
-            return False
-        year = int(data[2])
         if returndate:
-            return([month,day,year])
+            return [month,day,year]
         else:
             return True
     except:
         return False
     
 
+def dayssince0(date):
+    days = date[1]
+    for y in range(1, date[2]): 
+        if isleapyear(y):
+            days += 366 
+        else:
+            days += 365
+    for m in range(1, date[0]):
+        days += dayspermonth[m-1]
+        if m == 2:
+            days += 1
+    return days
+
+def printtimes(dsplnum = False):
+    for i in range(len(timers)):
+        time =  dayssince0(checkvaliddate(timers[i][1],True)) - dayssince0(today)
+        pen2.write(f"{f"{i+1}. " if dsplnum else ""}{timers[i][0]}: {time} day{"" if time == 1 else "s"} left",False,"center",("Arial",12,"normal"))
+        pen2.sety(pen2.ycor()-25)
+
+
 
 
 def addtimer(x,y):
     menucall(0)
-    timers.append(maketimer(getInput("Enter name of the timer.",letters),getInput("Enter ending date.\nIn mm/dd/yyyy",numbers,checkvaliddate),"pineapple"))
+    timers.append(maketimer(getInput("Enter name of the timer.",letters),
+                            getInput("Enter ending date.\nIn mm/dd/yyyy",numbers,checkvaliddate)))
     savethedate()
-    mainmenu(0,0)
+    mainmenu()
 
 def edittimer(x,y):
-    pass
+    menucall(0)
+    printtimes(True)
+    validnums = []
+    for i in range(len(timers)):
+        validnums.append(str(i+1))
+    selection = getInput("Enter the number of the Timer to Edit.",numbers,validnums)
+    print(selection)
 
 def deltimer(x,y):
     pass
@@ -159,8 +163,9 @@ def deltimer(x,y):
 
 
 
-def editmode(x,y):
+def editmode(x=0,y=0):
     pen.clear()
+    pen2.clear()
     menucall(4,[[(-200,0),(0,0),(200,0),(-300,-250)], # positions
                 [7,7,7,5], # sizes
                 ["Add Timer","Edit Timer","Delete Timer","Return"], # texts
@@ -186,10 +191,8 @@ def menucall(count,data=[]):
 
 
 
-def mainmenu(x,y):
-    for line in timers:
-        pen.write(line)
-        pen.sety(pen.ycor()-50)
+def mainmenu(x=0,y=0):
+    printtimes()
     pen.goto(0,200)
     menucall(1,[[(300,-250)], # positions
                 [5], # sizes
@@ -199,6 +202,43 @@ def mainmenu(x,y):
                 ])
 
 
-mainmenu(0,0)
+
+## ......... SETUP 2 ......... ##
+# setup - pen - text writing etcetera
+pen = trtl.Turtle()
+pen.hideturtle()
+pen.penup()
+pen.goto(0,200)
+
+pen2 = trtl.Turtle()
+pen2.hideturtle()
+pen2.penup()
+pen2.goto(0,200)
+# setup - buttons - turtles stored for clicking on later
+bts = []
+for i in range(10):
+    temp = trtl.Turtle(shape="circle")
+    temp.penup()
+    temp.hideturtle()
+    bts.append(temp)
+# setup - keyboard - makes lists of validated inputs for player input
+letters = list(string.printable) + ["Return","BackSpace"," ","Shift_L","Shift_R"]
+numbers = list(string.digits) + ["/","BackSpace","Return","Escape"]
+
+
+dayspermonth=[31,28,31,30,31,30,30,31,30,31,30,31]
+today = checkvaliddate(str(datetime.date.today()),True)
+
+
+
+timers = []
+with open("assets/timers/timerlist.txt","r") as file1:
+        for line in file1:
+            line = line.strip()
+            tmplist = line.split(";")
+            timers.append(tmplist)
+
+
+mainmenu()
 
 wn.mainloop()
